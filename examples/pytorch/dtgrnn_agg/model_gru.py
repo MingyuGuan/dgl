87d608cef9c1ea59e99406de9e7b6b0125753c99
Mingyu Guan <mingyu@mingyus-mbp.lan>
2021-10-26 12:58:33 -0400

    GraphGRU model: encoder handle seq inputs

--- examples/pytorch/dtgrnn_agg/model_gru.py
+++ examples/pytorch/dtgrnn_agg/model_gru.py
@@ -86,8 +86,8 @@ class StackedEncoder(nn.Module):
     # hidden_states should be a list which for different layer
     def forward(self, g, x, hidden_states):
         if self.aggregate_x:
-            x_split = torch.tensor_split(x, self.seq_len, dim=0)
-            print("Size of x_split", x_split.size())
+            x_split = torch.split(x, self.seq_len)
+            print("Size of x_split", len(x_split))
             x_cat = torch.cat(x_split, dim=-1)
             print("Size of x_cat", x_cat.size())
 
